a7424ba1421f153f0d15c2b49082fa884834ca11
pyliu <3634168+pyliu@users.noreply.github.com>
2021-10-05 22:31:45 +0800

    create upload folder if needed

--- server.js
+++ server.js
@@ -2,11 +2,20 @@
 const express = require('express')
 const fileUpload = require('express-fileupload')
 const cors = require('cors')
+const fs = require('fs')
+const path = require('path')
 
 const app = express()
 
+
+const dirName = 'upload'
+const uploadDir = path.join(__dirname, dirName)
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir)
+}
+
 // middle ware
-app.use(express.static('public')) //to access the files in public folder
+app.use(express.static(dirName)) //to access the files in `${dirName}` folder
 app.use(cors()) // it enables all cors requests
 app.use(fileUpload())
 
@@ -20,7 +29,7 @@ app.post('/upload', (req, res) => {
     const myFile = req.files.file
 
     //  mv() method places the file inside public directory
-    myFile.mv(`${__dirname}/public/${myFile.name}`, function (err) {
+    myFile.mv(`${__dirname}/${dirName}/${myFile.name}`, function (err) {
         if (err) {
             console.log(err)
             return res.status(500).send({ msg: "Error occured" })
