ce10154f6d7ac33aad48b5c39d12470d48e42922
pyliu <3634168+pyliu@users.noreply.github.com>
2021-11-20 14:56:03 +0800

    wip

---
+++ workers/user.js
@@ -0,0 +1,52 @@
+const path = require("path");
+const __basename = path.basename(__filename);
+const isEmpty = require("lodash/isEmpty");
+const { parentPort } = require("worker_threads");
+const config = require(path.join(__dirname, "..", "model", "config"));
+const MongoClient = require('mongodb').MongoClient;
+
+parentPort.on("message", async (postBody) => {
+  config.isDev && console.log("收到搜尋使用者訊息", postBody);
+  const client = new MongoClient(config.connUri);
+  let response = {
+    statusCode: config.statusCode.FAIL,
+    message: "未知的錯誤",
+    payload: undefined
+  };
+  try {
+    await client.connect();
+    config.isDev && console.log(__basename, '✔ DB已連線');
+    const userCollection = client.db().collection(config.userCollection);
+    
+    
+    const limit = postBody.limit || 0;
+    delete postBody.limit;
+    
+    const cursor = await caseCollection.find(postBody).sort({_id: -1});
+    limit && cursor.limit(limit);
+    const count = await cursor.count();
+    if (count === 0) {
+      const message =  "❔ 找不到資料。";
+      config.isDev && console.log(__basename, message);
+      response.statusCode = config.statusCode.FAIL_NOT_FOUND;
+      response.message = message;
+      response.payload = postBody;
+    } else {
+      // const cases = [];
+      // await cursor.forEach((element) => {
+      //   cases.push(element);
+      // });
+      const cases = await cursor.toArray();
+      const message = `🟢 找到 ${count} 案件`;
+      config.isDev && console.log(__basename, message);
+      response.statusCode = config.statusCode.SUCCESS;
+      response.message = message;
+      response.payload = cases;
+    }
+  } catch (e) {
+    console.error(__basename, '❗ 處理搜尋使用者執行期間錯誤', e);
+  } finally {
+    parentPort.postMessage(response);
+    await client.close();
+  }
+});
