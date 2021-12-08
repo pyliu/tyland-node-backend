64dd39a7aec7a810efc7f4adeb3a51273451172d
pyliu <3634168+pyliu@users.noreply.github.com>
2021-12-08 22:17:01 +0800

    add pm2 ecosystem.config.js

---
+++ ecosystem.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  apps: [{
+    name: 'tyland-sur-be',
+    exec_mode: 'cluster',
+    instances: 2,
+    script: './server.js',
+    args: 'start',
+    out_file: '../tyland-node-backend_out.log',
+    error_file: '../tyland-node-backend_err.log',
+    cron_restart: '0 7 * * *',
+    time: true,
+    watch: true,
+    ignore_watch: ['[/\\]./', 'node_modules', '.git'],
+    max_memory_restart: '512M',
+    env: {
+      NODE_ENV: 'production'
+    },
+    wait_ready: true,
+    restart_delay: 5000
+  }]
+}
