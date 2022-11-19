da7c4d973ad8c300ca91ae860994325b6bfb3c97
pyliu <3634168+pyliu@users.noreply.github.com>
2022-11-19 21:14:16 +0800

    new API warm up

--- server.js
+++ server.js
@@ -261,6 +261,20 @@ app.get("/stats/:site_code/uploaded", (req, res) => {
   }
 })
 
+app.get("/stats/:site_code/uploadedMongoDB", (req, res) => {
+  if (utils.authenticate(req.headers.authorization)) {
+    const worker = new Worker("./workers/statsUploadedMongoDB.js");
+    // listen to message to wait response from worker
+    worker.on("message", (data) => {
+      res.status(data.statusCode === config.statusCode.FAIL ? StatusCodes.NOT_ACCEPTABLE : StatusCodes.OK).send({ ...data });
+    });
+    // post data
+    worker.postMessage({ site_code: req.params.site_code });
+  } else {
+    res.status(StatusCodes.BAD_REQUEST).send({});
+  }
+})
+
 app.get("/stats/:site_code/cases", (req, res) => {
   if (utils.authenticate(req.headers.authorization)) {
     const worker = new Worker("./workers/statsCases.js");
